f05eaa15fe351b44b57a49be74cdd64d1c790c84
Taeyang Jin (Theo) <sun@dataportal.kr>
2026-04-18 07:56:31 +0000

    Updates

---
+++ component---src-pages-index-tsx-6f64a234583ca1129203.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunk_vallista_land_blog=self.webpackChunk_vallista_land_blog||[]).push([[245],{1543:function(e,t,a){a.r(t),a.d(t,{Head:function(){return f}});var i=a(2483),n=a(5428),r=a(9132),s=a(2053),l=a(278),o=a(2927),g=a(8664),d=a(1085);const m=n.i7`
+  0% {
+    transform: rotateZ(0);
+  }
+
+  50% {
+    transform: rotateZ(30deg);
+  }
+
+  100% {
+    transform: rotateZ(0deg);
+  }
+`,h=(0,i.A)("div",{target:"egaugi64"})({name:"8iutvn",styles:"margin-left:auto;margin-right:auto;max-width:900px;padding:2rem"}),u=(0,i.A)("header",{target:"egaugi63"})({name:"1j66vkv",styles:"padding:2rem 0"}),c=(0,i.A)("div",{target:"egaugi62"})("margin-bottom:1.5rem;max-width:550px;&>span:first-of-type{position:relative;&::after{position:absolute;right:-4rem;top:-0.5rem;content:'✋';display:block;animation:",m," 1s ease-in-out infinite;}}"),p=(0,i.A)("div",{target:"egaugi61"})({name:"1i7yo1v",styles:"max-width:550px;margin-bottom:2rem"}),x=(0,i.A)("section",{target:"egaugi60"})({name:"1jwuio6",styles:"width:100%;margin-left:auto;margin-right:auto;max-width:900px;padding:2rem"});t.default=e=>{const{data:t}=e,{nodes:a}=t.allMarkdownRemark;return(0,d.jsxs)(r.mc,{children:[(0,d.jsx)(u,{children:(0,d.jsxs)(h,{children:[(0,d.jsxs)(c,{children:[(0,d.jsx)(r.EY,{as:"span",size:48,weight:800,children:"안녕하세요,"}),(0,d.jsx)(r.EY,{as:"h1",size:48,weight:800,lineHeight:56,children:"진태양입니다."})]}),(0,d.jsx)(p,{children:(0,d.jsx)(r.EY,{as:"p",size:20,weight:400,lineHeight:40,children:"오랫동안 제품을 만들어왔고, 지금은 스타트업을 창업해 CTO로 일하고 있습니다. 그러면서 느끼고 생각한 것들을 이곳에 남깁니다."})}),(0,d.jsx)(r.$n,{size:"large",color:"alert",onClick:()=>{return e="/posts/",void(0,s.navigate)(e);var e},children:(0,d.jsx)(r.EY,{size:16,weight:800,children:"블로그 글 보러갈까요?"})})]})}),(0,d.jsx)(x,{children:(0,d.jsx)(l.V,{title:"최근 글",list:(i=a,(0,g.KF)(i).filter((e,t)=>t<6).map(e=>({name:e.frontmatter.title,slug:e.fields.slug,date:n(e.frontmatter.date)})))})})]});var i;function n(e){const[,t,a]=(0,g.WB)(e);return`${Number(t)}월 ${Number(a)}일`}};const f=e=>{let{location:t}=e;return(0,d.jsx)(o.G,{name:"테오 블로그 — 엔지니어링, Agentic AI, 에세이",pathname:t.pathname,noTemplate:!0})}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-6f64a234583ca1129203.js.map
